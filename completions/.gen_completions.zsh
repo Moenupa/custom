dc364102c3eb9fa213103364aac64fa7306aff43
Moenupa <49304833+Moenupa@users.noreply.github.com>
2026-03-15 02:54:20 +0800

    [clean] prepare for migration

--- completions/.gen_completions.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# gen shell completions
-uv generate-shell-completion zsh > $ZSH_CUSTOM/completions/_uv
-uvx --generate-shell-completion zsh > $ZSH_CUSTOM/completions/_uvx
-ruff generate-shell-completion zsh > $ZSH_CUSTOM/completions/_ruff
